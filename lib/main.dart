fc47238a83ca6e90456af678d030f425511300e6
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-08 09:14:40 +0100

    add a background color to the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: Colors.indigoAccent,
       ),
     );
   }
